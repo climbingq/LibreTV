e6adba7575669577a9df88f343b3b7b70705acab
climbingq <climbingq@163.com>
2025-05-14 15:53:07 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -12,7 +12,7 @@ const PASSWORD_CONFIG = {
 
 // 网站信息配置
 const SITE_CONFIG = {
-    name: 'LibreTV',
+    name: '倩倩专供',
     url: 'https://libretv.is-an.org',
     description: '倩倩专供',
     logo: './image/retrotv_5520.png',
